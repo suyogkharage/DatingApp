7199547e58907531b0b959b01d9007e8431f0796
Suyog Kharage <suyogkharage94@gmail.com>
2021-10-01 21:08:57 +0530

    End of section 5

---
+++ client/src/app/_models/user.ts
@@ -0,0 +1,4 @@
+export interface User {
+    usrename: string;
+    token: string;
+}
---
+++ client/src/app/register/register.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { AccountService } from '../_services/account.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+
+  @Output() cancelRegister = new EventEmitter();
+  model: any = {};
+
+  constructor(private accountService: AccountService) { }
+
+  ngOnInit() {
+  }
+
+  register() {
+    this.accountService.register(this.model)
+      .subscribe(arg => {
+        console.log(arg);
+        this.cancel();
+      }, error => {
+        console.log(error);
+      } );
+    
+  }
+
+  cancel() {
+    this.cancelRegister.emit(false);
+  }
+
+}
